bcb7feb5cd34246a0ca01b9135780fa96502b84b
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-11-14 13:55:08 -0500

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 const repoName = 'rise-of-a-yn'; // ← CHANGE THIS!
 
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProduction ? `/${repoName}` : '',
-  trailingSlash: true,
   reactStrictMode: true,
   turbopack: {},
 };
